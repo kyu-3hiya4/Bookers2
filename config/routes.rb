e13e83b5438df9ba975c0364fb600f8cd00b6ebd
kyu-3hiya4 <177545609+kyu-3hiya4>
2024-09-26 12:15:04 +0000

    [Add] route修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
- resources :users
- resources :books
  devise_for :users
+ resources :users, only: [:show, :edit, :update]
+ resources :books
+
  root to: "homes#top"
  get 'home/about', to: 'homes#about'
 
